f851e436e8825bc9f47c9e6b8f606918eaa507bc
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 01:06:47 +0000

    [automatic commit] 2025-06-20 01:06:47 UTC 175.35.1.30

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -32,7 +32,7 @@ CREATE TABLE Dogs (
 INSERT INTO Dogs (owner_id, name, size)
 VALUES
 ((SELECT user_id FROM Users WHERE username ='alice123'), 'Max', 'medium'),
-((SELECT user_id FROM Users WHERE username ='alice123'), 'Max', 'medium'),
+((SELECT user_id FROM Users WHERE username ='alice123'), 'Bella', 'medium'),
 ((SELECT user_id FROM Users WHERE username ='alice123'), 'Max', 'medium'),
 ((SELECT user_id FROM Users WHERE username ='alice123'), 'Max', 'medium'),
 ((SELECT user_id FROM Users WHERE username ='alice123'), 'Max', 'medium'),
