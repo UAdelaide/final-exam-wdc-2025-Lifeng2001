7b36ba4d6487e7086719e39a193c1fc1eee611bd
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 06:10:47 +0000

    [automatic commit] 2025-06-20 06:10:47 UTC 175.35.1.30

--- part1/db.js
+++ part1/db.js
@@ -27,7 +27,7 @@ async function initDB() {
                 password_hash VARCHAR(255) NOT NULL,
                 role ENUM('owner', 'walker') NOT NULL,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            )
+            ))
 
          ')
 await db.execute(
