602e2a346f4e3bc64459c5ea0b4deb9c3cdf98ca
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 17:53:18 +0000

    [automatic commit] 2025-06-20 17:53:18 UTC 175.35.1.30

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -18,16 +18,7 @@ router.get('/my-dogs', async (req, res) => {
       res.status(500).json({ error: 'Failed to fetch dogs' });
     }
   });
-  router.get('/my-dogs', async (req, res) => {
-    console.log('Session in /my-dogs:', req.session);
-
-     if (!req.session.user || !req.session.user.id) {
-        return res.status(401).json({ error: 'Not logged in' });
-      }
-
-      const ownerId = req.session.user.id;
-      console.log('ownerID',ownerId);
-
+  router.get('/', async (req, res) => {
     try {
       const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?',[ownerId]);
       res.json(rows);
