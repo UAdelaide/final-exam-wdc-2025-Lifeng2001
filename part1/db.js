30fd5133da1a1b7d7415438cff177939eba2919a
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 05:30:12 +0000

    [automatic commit] 2025-06-20 05:30:12 UTC 175.35.1.30

--- part1/db.js
+++ part1/db.js
@@ -1,39 +1,10 @@
-const mysql = require('mysql2/promise');
-const fs = require('fs');
-let db;
-
-async function initDatabase() {
-  try {
-    // Connect to MySQL without specifying a database
-    const connection = await mysql.createConnection({
-      // host: 'localhost',
+const mysql = require('mysql/promise');
+const pool = mysql.createPool({
+    // host: 'localhost',
 
     // user: 'root',
     // password: ''
-    // database: 'DogWalkService'
-    });
+    database: 'DogWalkService'
+});
 
-    // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
-    await connection.end();
-
-    // Now connect to the created database
-    db = await mysql.createConnection({
-      // host: 'localhost',
-
-    // user: 'root',
-    // password: ''
-    database: 'DogWalkService',
-    multipleStatements: true
-    });
-    const schemaSql = fs.readFileSync('./dogwalks.sql','utf8');
-    await db.query(schemaSql);
-    console.log('Database initialized successfully');
-} catch (err) {
-    console.error('Database initialization failed:', err);
-}
-}
-
-
-
-module.exports = {initDatabase,db: () => db};
+module.exports = pool;
