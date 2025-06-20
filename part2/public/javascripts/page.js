7401e207ad5ec3ea18ade10589ad5eadf892b486
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 12:46:55 +0000

    [automatic commit] 2025-06-20 12:46:55 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -216,7 +216,7 @@ function logout(){
     if (this.readyState ===4 ) {
         if (this.status ===200) {
             alert('logged out successfully');
-            window.location.href = '/login.index';
+            window.location.href = '/index.html';
         } else {
             alert('logout failed');
         }
