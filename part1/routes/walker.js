8ea29a85dff9af545b4ffffd640e42d9810c52d2
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 02:59:29 +0000

    [automatic commit] 2025-06-20 02:59:29 UTC 175.35.1.30

---
+++ part1/routes/walker.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+router.get('/api/walker/open', async(req, res) => {
+
+
+    const sql = `
+        SELECT w.request_id,
+        d.name AS dog_name,
+        w.requested_time,
+        w.duration_minutes,
+        w.location,
+        u.username AS owner_username
+        From WalkRequests w
+        JOIN Dogs d ON w.dog_id = d.dog_id
+        JOIN Users u ON d.owner_id = u.user_id
+        WHERE w.status = 'open'
+    `;
+
+    try {
+        const [results] = await db.query (sql);
+        res.json(results);
+    } catch (error) {
+        console.error('Error fetching open walk requests:', error);
+        res.status(500).json({ error: 'Failed to fetch walk requests'});
+    }
+});
+
+module.exports = router;
