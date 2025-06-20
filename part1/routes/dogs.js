4edc3a7aa5c7f68c052fe0e6039ab6cd58f34b14
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 02:09:45 +0000

    [automatic commit] 2025-06-20 02:09:45 UTC 175.35.1.30

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -5,6 +5,6 @@ const db = require('../db');
 router.get('/api/dogs',(req, res)=>) {
     try {
         const sql = 'SELECT d.name AS dog_name, d.size, u.username AS owner_username From Dogs d JOIN User ON d.owner_id = u.user_id';
-        
+        db.
     }
 }
