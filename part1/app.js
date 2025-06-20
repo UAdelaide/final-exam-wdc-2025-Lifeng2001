dd88907c332520629e79049afce6232486502bc9
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 05:35:07 +0000

    [automatic commit] 2025-06-20 05:35:07 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

--- part1/app.js
+++ part1/app.js
@@ -30,6 +30,13 @@ const database = require('./db');
 
 (async()=> {
     try {
+        INSERT INTO Users (username, email, password_hash, role)
+VALUES
+    ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+    ('bobwalker','bob@example.com', 'hashed456', 'walker'),
+    ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+    ('joes123','joes@example.com', 'hashed111','walker'),
+    ('momo123','momo@example.com', 'hashed222','owner');
 
 
     } catch (err) {
