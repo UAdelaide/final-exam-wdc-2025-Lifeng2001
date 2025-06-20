d13b2c81f766ab095760017a3d19f90d36cdcdfb
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 02:49:53 +0000

    [automatic commit] 2025-06-20 02:49:53 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -11,7 +11,7 @@ router.get('/api/dogs', async(req, res) => {
     // "location": "Parklands",
     // "owner_username": "alice123"
     const sql = `
-        SELECT d.name AS dog_name, d.size, u.username AS owner_username
+        SELECT w.name AS dog_name, d.size, u.username AS owner_username
         FROM Dogs d
         JOIN Users u ON d.owner_id = u.user_id
     `;
