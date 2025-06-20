803d3252edc8bb8151d6cac024e30ef01004bf74
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 00:57:58 +0000

    [automatic commit] 2025-06-20 00:57:58 UTC 175.35.1.30

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -13,7 +13,7 @@ INSERT INTO Users (username, emial, password_hash, role)
 VALUES (
     ('alice123', 'alice@example.com', 'hashed123', 'owner'),
     ('bobwalker','bob@example.com', 'hashed456', 'walke'),
-    ()
+    ('carol123', email carol@example.com, password hash hashed789, and role owner)
 
 )
 
