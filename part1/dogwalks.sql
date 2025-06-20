6e143669bbc144b581e115fb8776b7f271c38188
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 01:21:13 +0000

    [automatic commit] 2025-06-20 01:21:13 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -51,7 +51,7 @@ INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, sta
 VALUES
 ((SELECT dog_id FROM Dogs WHERE name = 'Max'),'2025-06-10 08:00:00',30,'Parklands', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Bella'),'2025-06-10 09:30:00',45,'Beachside Ave', 'accepted'),
-((SELECT dog_id FROM Dogs WHERE name = 'Bingo'),'2025-06-10 09:30:00',45,'Beachside Ave', 'accepted'),
+((SELECT dog_id FROM Dogs WHERE name = 'Bingo'),'2025-06-10 010:30:00',60,'Beachside Ave', 'accepted'),
 
 
 CREATE TABLE WalkApplications (
