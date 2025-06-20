57561c01731cd1d984924b6d653db3a8bd3cc0aa
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 03:33:09 +0000

    [automatic commit] 2025-06-20 03:33:09 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -23,12 +23,18 @@ router.get('/api/walkers/summary', async (req, res) => {
         COUNT(r.rating) AS total_ratings,
         AVG(r.rating) AS average_rating,
         COUNT(CASE WHEN wr.status = 'completed' THEN 1 END) AS completed_walks
+        const sql = `
+        SELECT u.username AS walker_username,
+        COUNT(r.rating) AS total_ratings,
+        AVG(r.rating) AS average_rating,
+        COUNT(CASE WHEN wr.status = 'completed' THEN 1 END) AS completed_walks
         FROM Users u
         LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
         LEFT JOIN WalkRequests wr ON u.user_id = wr.walker_id
          WHERE u.role = 'walker'
         GROUP BY u.user_id,u.username
         ORDER BY u.username
+    `;
     `;
 
     try {
