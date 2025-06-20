92c8a5a5d536fce3285c82b9fdbcb1dd7d803463
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 03:43:46 +0000

    [automatic commit] 2025-06-20 03:43:46 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -3,21 +3,6 @@ const router = express.Router();
 const db = require('../db');
 
 router.get('/api/walkers/summary', async (req, res) => {
-    // [
-    //     {
-    //       "walker_username": "bobwalker",
-    //       "total_ratings": 2,
-    //       "average_rating": 4.5,
-    //       "completed_walks": 2
-    //     },
-    //     {
-    //       "walker_username": "newwalker",
-    //       "total_ratings": 0,
-    //       "average_rating": null,
-    //       "completed_walks": 0
-    //     }
-    //   ]
-
     const sql = `
         SELECT u.username AS walker_username,
         COUNT(r.rating) AS total_ratings,
