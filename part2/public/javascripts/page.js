1b7fa862f2a3b58efe66c71d17d180c81314abc4
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 15:08:17 +0000

    [automatic commit] 2025-06-20 15:08:17 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -174,68 +174,42 @@ function downvote(index) {
 }
 
 
-// function login(){
+function login(){
 
-//     let user = {
-//         username: document.getElementById('username').value,
-//         password: document.getElementById('password').value
-//     };
+    let user = {
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value
+    };
 
-//     // Create AJAX Request
-//     var xmlhttp = new XMLHttpRequest();
+    // Create AJAX Request
+    var xmlhttp = new XMLHttpRequest();
 
-//     // Define function to run on response
-//     xmlhttp.onreadystatechange = function() {
-//         if (this.readyState == 4 && this.status == 200) {
+    // Define function to run on response
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
 
-//             const response = JSON.parse(this.responseText);
-//             alert("Welcome "+response.user.username);
-//             if(response.user.role ==='owner') {
-//                 window.location.href = '/owner-dashboard.html';
-//             } else if (response.user.role ==='walker'){
-//                 window.location.href = '/walker-dashboard.html';
+            const response = JSON.parse(this.responseText);
+            alert("Welcome "+response.user.username);
+            if(response.user.role ==='owner') {
+                window.location.href = '/owner-dashboard.html';
+            } else if (response.user.role ==='walker'){
+                window.location.href = '/walker-dashboard.html';
 
-//             }
-//         } else if (this.readyState == 4 && this.status >= 400) {
-//             alert("Login failed");
-//         }
-//     };
+            }
+        } else if (this.readyState == 4 && this.status >= 400) {
+            alert("Login failed");
+        }
+    };
 
-//     // Open connection to server & send the post data using a POST request
-//     // We will cover POST requests in more detail in week 8
-//     xmlhttp.open("POST", "/api/users/login", true);
-//     xmlhttp.withCredentials = true;
-//     xmlhttp.setRequestHeader("Content-type", "application/json");
-//     xmlhttp.send(JSON.stringify(user));
+    // Open connection to server & send the post data using a POST request
+    // We will cover POST requests in more detail in week 8
+    xmlhttp.open("POST", "/api/users/login", true);
+    xmlhttp.withCredentials = true;
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.send(JSON.stringify(user));
 
-// }
-async function login() {
-    try{
-      const response = await axios.post('/api/users/login',{
-        username: username.value,
-        password: password.value
-      });
+}
 
-      const role = response.data.user.role;
-      if (role === 'owner'){
-        window.location.href = 'owner-dashboard.html';
-      }
-      else if (role === 'walker'){
-        window.location.href = 'walker-dashboard.html';
-      }
-      else {
-        error.value = 'unknow role'
-      }
-    }
-    catch (err) {
-      if (err.response && err.response.data && err.response.data.error){
-        error.value = err.response.data.error;
-      }
-      else {
-        error.value = 'login failed'
-      }
-    }
-  }
 function logout(){
 
     // Create AJAX Request
