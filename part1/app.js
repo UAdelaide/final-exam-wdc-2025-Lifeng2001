543c5ee4d2dc5ac073dfbf8707f4c7288377d769
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 05:48:07 +0000

    [automatic commit] 2025-06-20 05:48:07 UTC 175.35.1.30

--- part1/app.js
+++ part1/app.js
@@ -30,7 +30,7 @@ app.use('/',walkersRouter);
 (async () => {
     try {
         // Connect to MySQL without specifying a database
-            const connection = await mysql.createConnection({
+            const db = await mysql.createConnection({
               host: 'localhost',
               user: 'root',
               password: '' // Set your MySQL root password
