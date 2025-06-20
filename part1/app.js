056e8628b1d5b837733e7210ddd65616f1a0b006
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 02:56:15 +0000

    [automatic commit] 2025-06-20 02:56:15 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

--- part1/app.js
+++ part1/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dogsRouter = require('./routes/dogs');
-var WaRouter = require('./routes/dogs');
+var walkrequestsRouter = require('./routes/wa');
 var app = express();
 
 app.use(logger('dev'));
