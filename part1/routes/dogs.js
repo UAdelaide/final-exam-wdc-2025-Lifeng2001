215ad2b27c1ff950aea653ac1b435ebc203601b6
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 02:14:35 +0000

    [automatic commit] 2025-06-20 02:14:35 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../db');
 
 router.get('/api/dogs',(req, res)=>) {
-    try {
+
         const sql = 'SELECT d.name AS dog_name, d.size, u.username AS owner_username From Dogs d JOIN User ON d.owner_id = u.user_id';
         db.query = (sql,(err,results)) => {
             if (err) {
@@ -12,5 +12,5 @@ router.get('/api/dogs',(req, res)=>) {
             }
             res.json(results);
         }
-    }
+    
 }
