e9d94581e4e5b4f3aa4eb14b619f3cbf9bf07955
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 01:16:12 +0000

    [automatic commit] 2025-06-20 01:16:12 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -49,7 +49,7 @@ CREATE TABLE WalkRequests (
 );
 INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status)
 VALUES
-((SELECT dog_id FROM ))
+((SELECT dog_id FROM Dogs WHERE name = 'Max'))
 
 CREATE TABLE WalkApplications (
     application_id INT AUTO_INCREMENT PRIMARY KEY,
