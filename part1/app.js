9e8ba1349fc10505ce554ac55e13535b81c588a4
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 03:10:36 +0000

    [automatic commit] 2025-06-20 03:10:36 UTC 175.35.1.30

--- part1/app.js
+++ part1/app.js
@@ -7,7 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dogsRouter = require('./routes/dogs');
 var walkrequestsRouter = require('./routes/walkrequests');
-var walkersRouter = require('./routes/walkrequests');
+var walkersRouter = require('./routes/walkers');
 var app = express();
 
 app.use(logger('dev'));
