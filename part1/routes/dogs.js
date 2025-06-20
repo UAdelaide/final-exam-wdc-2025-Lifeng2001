61992bc1db98a460acd410f5d784ebe4b58ad1a5
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 02:22:24 +0000

    [automatic commit] 2025-06-20 02:22:24 UTC 175.35.1.30

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -2,15 +2,16 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.get('/api/dogs',(req, res) =>  {
+router.get('/api/dogs', (req, res) => {
 
-        const sql = 'SELECT d.name AS dog_name, d.size, u.username AS owner_username From Dogs d JOIN User ON d.owner_id = u.user_id';
-        db.query = (sql,(err,results)) => {
-            if (err) {
-                console.error('error:',err);
-                return res.status(500).json({error: 'Failed to fetch dogs'});
-            }
-            res.json(results);
-        });
+    const sql = 'SELECT d.name AS dog_name, d.size, u.username AS owner_username
+    From Dogs d JOIN User ON d.owner_id = u.user_id';
+    db.query = (sql, (err, results)) => {
+        if (err) {
+            console.error('error:', err);
+            return res.status(500).json({ error: 'Failed to fetch dogs' });
+        }
+        res.json(results);
+    });
 
 });
