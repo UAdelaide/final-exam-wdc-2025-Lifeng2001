f5336600177c222d27f62c7e11b10b089e02ea82
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 17:52:58 +0000

    [automatic commit] 2025-06-20 17:52:58 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -18,6 +18,23 @@ router.get('/my-dogs', async (req, res) => {
       res.status(500).json({ error: 'Failed to fetch dogs' });
     }
   });
+  router.get('/my-dogs', async (req, res) => {
+    console.log('Session in /my-dogs:', req.session);
+
+     if (!req.session.user || !req.session.user.id) {
+        return res.status(401).json({ error: 'Not logged in' });
+      }
+
+      const ownerId = req.session.user.id;
+      console.log('ownerID',ownerId);
+
+    try {
+      const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?',[ownerId]);
+      res.json(rows);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to fetch dogs' });
+    }
+  });
   module.exports = router;
 
 
