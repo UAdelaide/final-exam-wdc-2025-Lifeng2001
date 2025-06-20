1cf23a268448c7e417235839cb6babe06e6911fd
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 13:40:38 +0000

    [automatic commit] 2025-06-20 13:40:38 UTC 175.35.1.30

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
     }
 
     try {
-      const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ? ');
+      const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?',[ownerId]);
       res.json(rows);
     } catch (error) {
       res.status(500).json({ error: 'Failed to fetch users' });
