721deae9f20708f72243d409cf6f40b6f0889f1a
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 02:47:21 +0000

    [automatic commit] 2025-06-20 02:47:21 UTC 175.35.1.30

---
+++ part1/routes/walkrequests.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+router.get('/api/dogs', async(req, res) => {
+    const sql = `
+        SELECT d.name AS dog_name, d.size, u.username AS owner_username
+        FROM Dogs d
+        JOIN Users u ON d.owner_id = u.user_id
+    `;
+
+    try {
+        const [results] = await db.query (sql);
+        res.json(results);
+    } catch (error) {
+        console.error('Error fetching dogs:', error);
+        res.status(500).json({ error: 'Failed to fetch dogs'});
+    }
+});
+
+module.exports = router;
