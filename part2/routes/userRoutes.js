7d5a549a6f385c35e2d716b352e21a920b0fd5c6
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 11:12:16 +0000

    [automatic commit] 2025-06-20 11:12:16 UTC 175.35.1.30

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,8 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
     req.session.user = {
-      user_id:rows[0].user_id,
+      user_id: rows[0].user_id,
+      username:rows[0].username
 
     }
 
