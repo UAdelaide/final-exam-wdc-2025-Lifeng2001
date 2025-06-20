4555243a6254cc96aadbf1b0b1f738163d118425
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 03:23:24 +0000

    [automatic commit] 2025-06-20 03:23:24 UTC 175.35.1.30

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -24,7 +24,7 @@ router.get('/api/walkers/summary', async(req, res) => {
         AVG(r.rating) AS average_rating,
         COUNT(CASE WHEN wk.status = 'completed' THEN 1 END) AS completed_walks
         FROM Users u
-        LEFT
+        LEFT JOIN WalkRating r ON u.user
         GROUP BY w.walker_id,w.username
         ORDER BY w.username
     `;
