d2ae76b3a0c1a14a135ea9ae71d05c695afd42b7
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 02:39:42 +0000

    [automatic commit] 2025-06-20 02:39:42 UTC 175.35.1.30

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.get('/', async(req, res) => {
+router.get('/api/dogs', async(req, res) => {
     const sql = `
         SELECT d.name AS dog_name, d.size, u.username AS owner_username
         FROM Dogs d
