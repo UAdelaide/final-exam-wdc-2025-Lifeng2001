079b0ae422f48228d881742a06e9b13aa7f54ff2
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 03:03:33 +0000

    [automatic commit] 2025-06-20 03:03:33 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -21,7 +21,7 @@ router.get('/api/walker/summary', async(req, res) => {
     const sql = `
         SELECT w.walker_username AS walker_username,
         COUNT(r.rating AS total_ratings,
-        
+        AVG(r.rating) AS average_rating
     `;
 
     try {
