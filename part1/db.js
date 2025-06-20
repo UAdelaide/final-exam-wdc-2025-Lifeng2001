08d0401bff018a9eb6d4c25a9c46fdca4a91d6b1
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 06:17:19 +0000

    [automatic commit] 2025-06-20 06:17:19 UTC 175.35.1.30

--- part1/db.js
+++ part1/db.js
@@ -79,8 +79,7 @@ async function initDB() {
                 FOREIGN KEY (walker_id) REFERENCES Users(user_id),
                 FOREIGN KEY (owner_id) REFERENCES Users(user_id),
                 CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
-                
-
+            )
         ');
 
 
