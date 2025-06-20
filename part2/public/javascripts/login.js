4c02cf2552891db4a862b6f0ea34f6655154e633
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 10:04:55 +0000

    [automatic commit] 2025-06-20 10:04:55 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

---
+++ part2/public/javascripts/login.js
@@ -0,0 +1,29 @@
+function login(){
+
+    let user = {
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value
+    };
+
+    // Create AJAX Request
+    var xmlhttp = new XMLHttpRequest();
+
+    // Define function to run on response
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+            alert("login success");
+            alert("Welcome "+this.responseText);
+            window.location.href = '/owner-dashboard.html';
+        } else if (this.readyState == 4 && this.status >= 400) {
+            alert("Login failed");
+        }
+    };
+
+    // Open connection to server & send the post data using a POST request
+    // We will cover POST requests in more detail in week 8
+    xmlhttp.open("POST", "/api/users/login", true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.send(JSON.stringify(user));
+
+}
