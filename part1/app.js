b54c826c4cbc9e1c0ade20bbcf05f5201e6eadd6
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 05:35:43 +0000

    [automatic commit] 2025-06-20 05:35:43 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part1/app.js
+++ part1/app.js
@@ -39,6 +39,29 @@ VALUES
     ('momo123','momo@example.com', 'hashed222','owner');
 
 
+
+
+    INSERT INTO Dogs (owner_id, name, size)
+VALUES
+((SELECT user_id FROM Users WHERE username ='alice123'), 'Max', 'medium'),
+((SELECT user_id FROM Users WHERE username ='carol123'), 'Bella', 'small'),
+((SELECT user_id FROM Users WHERE username ='alice123'), 'Bingo', 'large'),
+((SELECT user_id FROM Users WHERE username ='carol123'), 'Tmood', 'small'),
+((SELECT user_id FROM Users WHERE username ='momo123'), 'Stake', 'medium');
+
+
+
+
+
+INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status)
+VALUES
+((SELECT dog_id FROM Dogs WHERE name = 'Max'),'2025-06-10 08:00:00',30,'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Bella'),'2025-06-10 09:30:00',45,'Beachside Ave', 'accepted'),
+((SELECT dog_id FROM Dogs WHERE name = 'Bingo'),'2025-06-10 10:30:00',60,'North Terrace', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Tmood'),'2025-06-10 12:00:00',30,'Torrens Ave', 'cancelled'),
+((SELECT dog_id FROM Dogs WHERE name = 'Stake'),'2025-06-10 14:00:00',50,'South Tce', 'completed');
+
+
     } catch (err) {
         console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
     }
