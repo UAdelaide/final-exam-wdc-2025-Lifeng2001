8a10b74bc138e05d6f92454351dc176483c09c2e
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 15:25:40 +0000

    [automatic commit] 2025-06-20 15:25:40 UTC 175.35.1.30

--- part2/app.js
+++ part2/app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(session({
     secret:'123',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
     maxAge: 1000 * 60 * 60 * 24
 }));
 
