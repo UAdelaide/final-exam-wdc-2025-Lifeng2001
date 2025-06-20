6c7bdc9a1028a76ff553209a677dcc2f97f4b7d0
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 13:46:24 +0000

    [automatic commit] 2025-06-20 13:46:24 UTC 175.35.1.30

--- part2/app.js
+++ part2/app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const walkRoutes = require('./routes/walkRoutes');
+
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
