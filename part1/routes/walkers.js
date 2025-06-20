7e2dd5d4a78722226ac2e2b0b30bdc9ff43d0503
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 03:42:04 +0000

    [automatic commit] 2025-06-20 03:42:04 UTC 175.35.1.30

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -24,8 +24,8 @@ router.get('/api/walkers/summary', async (req, res) => {
         AVG(r.rating) AS average_rating,
         COUNT(CASE WHEN wr.status = 'completed' THEN 1 END) AS completed_walks
         FROM Users u
-        LEFT JOIN WalkRatings r ON u.user_id= r.walker
-        LEFT JOIN WalkRequests wr ON u.user_id = wr.walker_id
+        LEFT JOIN WalkRatings r ON u.user_id= r.walker_id
+        LEFT JOIN WalkRequests wr ON u.user_ = wr.walker_id
         WHERE u.role = 'walker'
         GROUP BY u.user_id,u.username
         ORDER BY u.username
