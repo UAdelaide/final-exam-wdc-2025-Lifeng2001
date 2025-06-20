53c00b6c8ec92393d4a2d9903f55671407101dce
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 01:12:59 +0000

    [automatic commit] 2025-06-20 01:12:59 UTC 175.35.1.30

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -47,7 +47,7 @@ CREATE TABLE WalkRequests (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
 );
-INSERT INTO WalkRequests(owner_id, name, size)
+INSERT INTO WalkRequests(do, name, size)
 
 CREATE TABLE WalkApplications (
     application_id INT AUTO_INCREMENT PRIMARY KEY,
