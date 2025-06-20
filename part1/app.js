6c7b9629b77f0c29da2609ec2d7109bbf34df537
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 04:53:37 +0000

    [automatic commit] 2025-06-20 04:53:37 UTC 175.35.1.30

--- part1/app.js
+++ part1/app.js
@@ -26,10 +26,11 @@ app.use(express.static(path.join(__dirname, 'public')));
         app.use('/', indexRouter);
         app.use('/users', usersRouter);
         app.use('/',dogsRouter);
-app.use('/',walkrequestsRouter);
-app.use('/',walkersRouter);
+        app.use('/',walkrequestsRouter);
+        app.use('/',walkersRouter);
     }
-})
+});
+
 
 
 
