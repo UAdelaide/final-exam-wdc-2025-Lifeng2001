f1b426bc60b261258a8c02fd4888048e6946a56e
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 05:44:07 +0000

    [automatic commit] 2025-06-20 05:44:07 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

--- part1/app.js
+++ part1/app.js
@@ -106,10 +106,24 @@ CREATE TABLE WalkRatings (
         `);
 
         await db.execute(`
-            INSERT INTO books (title, author) VALUES
-            ('1984', 'George Orwell'),
-            ('To Kill a Mockingbird', 'Harper Lee'),
-            ('Brave New World', 'Aldous Huxley')
+            INSERT INTO Users (username, email, password_hash, role)
+VALUES
+    ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+    ('bobwalker','bob@example.com', 'hashed456', 'walker'),
+    ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+    ('joes123','joes@example.com', 'hashed111','walker'),
+    ('momo123','momo@example.com', 'hashed222','owner');
+          `);
+
+
+          await db.execute(`
+            INSERT INTO Users (username, email, password_hash, role)
+VALUES
+    ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+    ('bobwalker','bob@example.com', 'hashed456', 'walker'),
+    ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+    ('joes123','joes@example.com', 'hashed111','walker'),
+    ('momo123','momo@example.com', 'hashed222','owner');
           `);
 
 
