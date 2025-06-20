6a2eb422825fb78f2b4fe08be0c4446a7c7bb4b6
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 05:10:11 +0000

    [automatic commit] 2025-06-20 05:10:10 UTC 175.35.1.30

--- part1/db.js
+++ part1/db.js
@@ -27,7 +27,7 @@ async function initDatabase() {
     multipleStatements:true
     });
     const schemaSql = require('fs').readFileSync('./dogwalks.sql','utf8');
-    await db.query(schema)
+    await db.query(schemaSql);
     console.log('Database initialized successfully');
 } catch (err) {
     console.error('Database initialization failed:', err);
