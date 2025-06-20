0a2e7ddd08d64075e8261a6e7796ea4c3e4e2eb9
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 03:02:02 +0000

    [automatic commit] 2025-06-20 03:02:02 UTC 175.35.1.30

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -19,7 +19,7 @@ router.get('/api/walker/summary', async(req, res) => {
     //   ]
 
     const sql = `
-        SELECT w.walker_username,
+        SELECT w.walker_username AS walker_user,
         d.name AS dog_name,
         w.requested_time,
         w.duration_minutes,
