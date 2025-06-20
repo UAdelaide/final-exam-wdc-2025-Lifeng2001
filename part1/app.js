1c0be002ef39be80ff4ddc0e52863faa47560fa2
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 05:29:26 +0000

    [automatic commit] 2025-06-20 05:29:26 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

--- part1/app.js
+++ part1/app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const {initDatabase, db} = require('./db')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 (async()=> {
     try {
-        await initDatabase();
+        
         app.use('/', indexRouter);
         app.use('/users', usersRouter);
         app.use('/',dogsRouter);
