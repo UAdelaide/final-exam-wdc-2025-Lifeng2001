ef44da7ba8892ef3f77cde8bd1e938ba1c74caeb
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 05:40:58 +0000

    [automatic commit] 2025-06-20 05:40:58 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.1.30

--- part1/app.js
+++ part1/app.js
@@ -44,7 +44,15 @@ const database = require('./db');
                   password: '',
                   database: 'DogWalkService'
                 });
-            
+
+            await db.execute(`
+                CREATE TABLE IF NOT EXISTS books (
+                      id INT AUTO_INCREMENT PRIMARY KEY,
+                      title VARCHAR(255),
+                      author VARCHAR(255)
+                    )
+                `);
+
 
 
 
