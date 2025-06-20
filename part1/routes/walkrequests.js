575f593bef9854c8946f497f26a89bc9d2a4d6a7
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 06:02:53 +0000

    [automatic commit] 2025-06-20 06:02:53 UTC 175.35.1.30

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const { getDb } = require('../db');
 
 router.get('/api/walkrequests/open', async(req, res) => {
 
