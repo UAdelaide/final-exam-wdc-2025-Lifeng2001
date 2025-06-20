613bad669b9c50ad3a36c77a229a6c79ef03e989
Lifeng2001 <a1864784@student.adelaide.edu.au>
2025-06-20 01:04:50 +0000

    [automatic commit] 2025-06-20 01:04:50 UTC 175.35.1.30

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -31,7 +31,7 @@ CREATE TABLE Dogs (
 
 INSERT INTO Dogs (owner_id, name, size)
 VALUES
-((SELECT user_id FROM Users WHERE user))
+((SELECT user_id FROM Users WHERE username =''))
 
 CREATE TABLE WalkRequests (
     request_id INT AUTO_INCREMENT PRIMARY KEY,
